2858761f18ca0e6e0aad1b949762c444514fb74a
Subhadeep298 <subhadeepbanerjee298@gmail.com>
2021-02-06 20:17:12 +0530

    new page

---
+++ for_ino/script.js
@@ -0,0 +1,11 @@
+var b=prompt("Enter password");
+  	if( b=="2108")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("invalid details");
+  		window.location.href="index.html";
+  	}
+  	
